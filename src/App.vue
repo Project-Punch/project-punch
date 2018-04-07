63a99424cc3cdf8689d58fd21f8dc25bc6e2fc32
Nico Franke <ZerNico.yt@gmail.com>
2018-04-07 12:50:56 +0200

    Add basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,53 @@
 <template>
   <v-app>
-    <router-view/>
+    <v-navigation-drawer
+      fixed
+      :clipped="$vuetify.breakpoint.lgAndUp"
+      app
+      v-model="drawer"
+    >
+      <v-list dense v-for="item in items" :key="item.text">
+        <v-list-tile @click="" :to="item.link">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.text }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar
+      color="primary"
+      dark
+      app
+      :clipped-left="$vuetify.breakpoint.lgAndUp"
+      fixed
+    >
+      <v-toolbar-title class="ml-0 pl-3">
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <span class="hidden-sm-and-down">Project Punch</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-content>
+      <router-view/>
+    </v-content>
   </v-app>
 </template>
+
 
 <script>
 export default {
   data () {
     return {
+      drawer: null,
+      items: [
+        { icon: 'smartphone', text: 'Phones', link: '/' },
+        { icon: 'apps', text: 'Apps/Tools', link: 'apps' },
+        { icon: 'web', text: 'Blog',  link: '/blog' },
+        { icon: 'info_outline', text: 'About', link: '/about' }
+      ]
     }
   }
 }
